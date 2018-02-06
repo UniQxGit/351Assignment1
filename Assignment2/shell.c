b44ea874626c54fb1154df881913e6f5d023f2ef
UniQx <31258420+UniQxGit@users.noreply.github.com>
2018-02-05 19:56:57 -0800

    Bug fix where spaces before or after would break the command

--- Assignment2/shell.c
+++ Assignment2/shell.c
@@ -15,6 +15,7 @@
 // forward declaration
 long read_command(char commandBuffer[]);
 void parse_command(char commandBuffer[], char *args[], long length);
+void clean_input(char commandBuffer[], long length);
 
 int main() {
     char commandBuffer[MAX_LINE]; // command buffer
@@ -59,7 +60,7 @@ int main() {
                     perror("Could not fork the process");
                     break;
                 case 0: //child process
-                    printf("Child running(%d): command = %s args = %s\n",child,commandBuffer,*args);
+                    printf("Child running(%d): command = %s. args = %s.\n",child,commandBuffer,*args);
                     status = execlp(commandBuffer,commandBuffer,*args,NULL);
                     if(status != 0)
                     {
@@ -118,6 +119,41 @@ long read_command(char commandBuffer[]) {
     return len;
 }
 
+
+//Cleans the input so that spaces can be put in before and after, in order to circumvent the "invalid path" error.
+void clean_input(char commandBuffer[], long length)
+{
+    long leftIndex = 0;
+    long rightIndex = length-2;
+
+    int canLeft = 1;
+    int canRight = 1;
+    
+    char cmp = ' ';
+    
+    for(int i = 0; i < length-1; i++)
+    {
+        if(commandBuffer[leftIndex] == ' ')
+        {
+            leftIndex++;
+        }
+
+        if(commandBuffer[rightIndex] == ' ')
+        {
+            rightIndex--;   
+        }
+    }
+
+    int cIndex;
+    for(int i = leftIndex; i <= rightIndex; i++)
+    {
+        commandBuffer[cIndex] = commandBuffer[i];
+        cIndex++;
+    }
+
+    commandBuffer[cIndex] = NULL;
+}
+
 // TODO: Complete this function to set up the arguments, args, to pass
 //       to the execvp system call
 void parse_command(char commandBuffer[], char *args[], long length) {
@@ -127,15 +163,24 @@ void parse_command(char commandBuffer[], char *args[], long length) {
     int ct = 0;
     
     int ix;
+
+
+    clean_input(commandBuffer,length);
+
     for (ix = 0; ix < length; ix++) { // parse command buffer
         /* examine every character in the commandBuffer */
         switch (commandBuffer[ix]) {
             case ' ': case '\t':
                 // TODO: Setup the pointer for the arguments
                 // insert your code here
-
-                start = ix;
-                commandBuffer[ix] = '\0';
+                if(start == -1)
+                {
+                    start = ix;
+                    commandBuffer[ix] = '\0';    
+                }else if(start = ix-1){
+                    start = ix;
+                }
+                
                 break;
                 
             case '\n':
@@ -150,10 +195,9 @@ void parse_command(char commandBuffer[], char *args[], long length) {
                 // TODO: Set up the 'start' index
                 // insert your code here
                 
-                if(start != -1)
+                if(start > 0)
                 {
                     args[ct] = &commandBuffer[ix];
-                    printf("Args[%d]:%s\n",ct,args[ct]);
                     ct++;
                 }
                 // Stop the process if the user enters a background task
@@ -165,5 +209,4 @@ void parse_command(char commandBuffer[], char *args[], long length) {
     }
     
     args[ct] = NULL; // just in case the command was > 80 characters
-    printf("Parsed. args: %s\n",*args);
 }
