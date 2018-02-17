1bfe6d939c627c060b6631acdcc1446128a4cd33
UniQx <31258420+UniQxGit@users.noreply.github.com>
2018-02-17 10:52:51 -0800

    Began Assignment 3

---
+++ Assignment3/exam_eval.c
@@ -0,0 +1,99 @@
+//
+//  exam_eval.c
+//  Assignment3_OrdinaryPipes
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define BUFFER_SIZE 100
+#define READ_END    0
+#define WRITE_END   1
+
+// Function prototypes
+char isExamHard(void);
+void request_rating(int firstPipe[], int secondPipe[]);
+void respond_to_the_rating(int firstPipe[], int secondPipe[]);
+
+int main(int argc, const char * argv[]) {
+   pid_t pid = fork();
+   // TODO: Create two pipes
+   //       Terminate the program if fail to create the pipes
+   
+
+   // TODO: Create a child process
+   //       Terminate the program if fail to fork a child process
+
+   // TODO: parent process invokes request_rating
+   //       child process invokes respond_to_the_rating
+   if(pid == 0)
+   {
+      //request_rating();  
+   }else if(pid >0)
+   {
+      //respond_to_the_rating();
+   }else{
+      perror("Could not fork the process");
+      exit(-1);
+   }
+   
+   
+   return 0;
+}
+
+/*********************************************************
+ *                        isExamHard                     *
+ * This function asks a question about Exam 1 difficulty *
+ ********************************************************/
+char isExamHard(void)
+{
+   char input;
+   printf("How would you rate the difficulty of Exam 1?\n");
+   printf("Enter E for Easy, O for ok, H for Hard: ");
+   scanf("%c", &input);
+   
+   return input;
+}
+
+/******************************************************
+ *                    request_rating                  *
+ * Asks the student to rate the exam #1, sends the    *
+ * student's reponse to the second process and reads  *
+ * the message from the child process.                *
+ *****************************************************/
+void request_rating(int *first_pipe, int *second_pipe) {
+   char level;
+   char write_msg[BUFFER_SIZE];
+   
+   level = isExamHard();
+   
+   printf("answer: %c\n", level);
+   if ((level == 'E') || (level == 'e')) {
+      strcpy(write_msg, "easy");
+   }
+   else if ((level == 'O') || (level == 'o')) {
+      strcpy(write_msg, "just right");
+   }
+   else {
+       strcpy(write_msg, "hard");
+   }
+   printf("easy or hard: %s\n", write_msg);
+   
+   // TODO: write the student's response to the first pipe
+
+   // TODO: read the message from the second pipe
+}
+
+/**********************************************************
+ *             respond_to_the_rating                      *
+ * Reads the message from the first process and returns   *
+ * the new message with the student's response to the     *
+ * first process.                                         *
+ *********************************************************/
+void respond_to_the_rating(int *first_pipe, int *second_pipe) {
+   // TODO: read the student's response from the first pipe
+   
+   // TODO: write the new message to the second pipe
+}
